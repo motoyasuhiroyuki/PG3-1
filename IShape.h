ab3ad31452536e1ca14a8663558b2fb07e071adc
MSI\裕之 <k021g1294@g.neec.ac.jp>
2023-02-01 13:42:13 +0900

    PG3_3-6

---
+++ IShape.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class IShape //インターフェイス
+{
+public: 
+virtual void size() = 0;
+virtual void draw() = 0;
+};
+
--- main.cpp
+++ main.cpp
@@ -1,22 +1,16 @@
-#include "Enemy.h"
-#include <stdio.h>
+ï»¿#include "Circle.h"
+#include "Rectangle.h"
+int main()
+{
+Circle circle;
+Rectangle rectangle;
 
+circle.size();
+circle.draw();
 
+rectangle.size();
+rectangle.draw();
 
-int main() {
-	Enemy* enemy1 = new Enemy();
-	int input;
+return 0;
 
-	while (true) {
-		
-		//“ü—Íˆ—
-		printf("0 : exit  1 : continue ==> ");
-		scanf_s("%d", &input);
-		if (input == 0) break; 
-			enemy1->Update();
-			enemy1->Draw();
-	}
-	delete enemy1;
-	
-	return 0;
 }
